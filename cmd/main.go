cc5521c605f299b166017d9364476a1f5fbf66a8
Artem <dzaparidzeartem@gmail.com>
2025-12-09 20:20:35 +0500

    feat: initialize project structure and entry point
    Set up initial Go project structure with main entry point, app runner, and placeholder files for domain, handler, repository, and service layers. Updated .gitignore to include .idea directory.

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/airsss993/russia-heroes-backend/internal/app"
+
+func main() {
+	app.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/airsss993/russia-heroes-backend
+
+go 1.25.0
